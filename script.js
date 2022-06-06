e27f848c05cf59014eda800a6a59b8cf91ae60ed
offnsivapps <105620515+offnsivapps@users.noreply.github.com>
2022-06-06 12:04:55 +1000

    Update script.js

--- script.js
+++ script.js
@@ -372,6 +372,8 @@ class Game {
         let newBlocks = currentBlock.place();
         this.newBlocks.remove(currentBlock.mesh);
         if (newBlocks.placed)
+          var audio = new Audio('img/ding1.wav');
+                audio.play()
             counter = counter + 1;
             if(counter == 5 ){
                 this.aboutme.innerHTML = strings1.random()
